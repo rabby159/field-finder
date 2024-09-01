5bb7c07cec375ce1d3b483505cda36b721960174
Md Rabby <ablazerabbi159@gmail.com>
2024-09-02 01:22:39 +0600

    route and server setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import router from './app/routes'
+
+const app: Application = express()
+
+//parser
+app.use(express.json())
+app.use(cors())
+
+//application routes
+
+// application routes
+app.use('/api/v1', router)
+
+const test = (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
+}
+
+app.get('/', test)
+
+export default app
