148b8407ebe654547f1d35a7aaa88f80421c6dfd
Md Rabby <ablazerabbi159@gmail.com>
2024-09-02 02:09:00 +0600

    eslinet setup complete

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,6 @@ const router = Router();
 
 const moduleRoutes = [{}];
 
-moduleRoutes.forEach((route) => router.use(route.path, route.route))
+// moduleRoutes.forEach((route) => router.use(route.path, route.route))
 
-export default router
+export default router;
