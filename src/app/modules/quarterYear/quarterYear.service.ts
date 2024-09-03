1c9834cca157f14a872b23154c0c15044b528eb1
Md Rabby <ablazerabbi159@gmail.com>
2024-09-04 02:28:44 +0600

    quarter year service for create

---
+++ src/app/modules/quarterYear/quarterYear.service.ts
@@ -0,0 +1,17 @@
+import { quarterYearNameCodeMapper } from './quarterYear.constant'
+import { TQuarter } from './quarterYear.interface'
+import { QuarterYear } from './quarterYear.model'
+
+const createQuarterYearIntoDB = async (payload: TQuarter) => {
+  if (quarterYearNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid quarter code. Please Enter right code')
+  }
+
+  const result = await QuarterYear.create(payload)
+
+  return result
+}
+
+export const AcademicSemesterServices = {
+  createQuarterYearIntoDB,
+}
