d6eb0efe3b301fbbb1e47743d12e73824cc5ffea
Md Rabby <ablazerabbi159@gmail.com>
2024-09-04 01:16:28 +0600

    controller for creat User

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,18 @@
-
+import catchAsync from '../../utils/catchAsync'
+import sendResponse from '../../utils/sendResponse'
+import httpStatus from 'http-status'
 
 const createUser = catchAsync(async (req, res) => {
-    const { password, student: studentData } = req.body
-  
-    //will we call services function to send this data
-    const result = await UserServices.createStudentIntoDB(password, studentData)
-  
-    //send response
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is created successfully',
-      data: result,
-    })
+  const { password, user: userData } = req.body
+
+  //will we call services function to send this data
+  const result = await UserServices.createStudentIntoDB(password, userData)
+
+  //send response
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is created successfully',
+    data: result,
   })
+})
